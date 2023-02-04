568ec06d6c8ded56d01ae19c95ae5a3014c31530
gfouz <gfouz1975@gmail.com>
2023-02-04 00:52:16 -0500

    fixing package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gfouz-overview/",
+  base: "/react-portfolio/",
   plugins: [react(), tsconfigPaths()],
 });
